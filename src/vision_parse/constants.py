f0f0d18a80e1ee6d05dda57732a342fde274627b
leesangboo <lsb@kopens.com>
2025-05-01 15:59:07 +0900

    ...

--- src/vision_parse/constants.py
+++ src/vision_parse/constants.py
@@ -8,4 +8,6 @@ SUPPORTED_MODELS: Dict[str, str] = {
     "gpt-4o": "openai",
     "gpt-4o-mini": "openai",
     "gemini-2.0-flash": "gemini",
+    "gemini-2.5-flash-preview-04-17" : "gemini",   
+    "gemini-2.5-pro-preview-03-25" : "gemini",
 }
